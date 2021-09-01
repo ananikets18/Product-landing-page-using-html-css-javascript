396b99830cbd39188ed6156dd2cce53658a9b721
Aniket Shinde <57999016+ananikets18@users.noreply.github.com>
2021-09-01 19:35:03 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+gsap.from('nav', {opacity:0, delay:1.6, duration:2.5, ease: "elastic.out(1,0.3)", y:-100});
+
+gsap.from('.search', {opacity:0, delay:1.8, duration:2.8, ease: "elastic.out(1,0.3)", y:-100});
+
+gsap.from('.brand_type', {opacity:0, delay:2.2, duration:3, ease: "elastic.out(1,0.3)", y:-100});
+
+gsap.from('.discount', {opacity:0, delay:1, scale:.1, duration:2.4, rotation:180,
+ ease: "expo.inOut", x:100});
+
+gsap.from('.bottom_shoes_card', {opacity:0, delay:2.6, duration:3.5, ease: "elastic.out(1,0.3)", x:100});
+
+gsap.from('.selected_product_img', {opacity:0, delay:3, duration:4, ease:"elastic.out(1,0.3)", y:100});
+
+gsap.from('.size_product', {opacity:0, delay:3.5, duration:0.5, ease:"Expo.In", y:100});
+
+gsap.from('.circle1', {opacity:0, delay:4.5, scale:.1, duration:.7, ease:"expo.In"});
+
+gsap.from('.circle2', {opacity:0, delay:4.8, scale:.1, duration:.7, ease:"expo.In"});
+
+
+
+
+
